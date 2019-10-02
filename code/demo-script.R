37d9a1de6e9b9600c0043112a5d19b51bf13143b
Marta Carrizo Vaque <marta.carrizo@berkeley.edu>
2019-10-02 15:11:38 -0700

    code: add demo-script.R

---
+++ code/demo-script.R
@@ -0,0 +1,19 @@
+# ===================================================================
+# Title: Lab 05
+# Description:
+#   This script performs what it asked on
+#   lab05 of STAT 133
+# Input(s): data file 'nba2018-players.csv'
+# Output(s): summary data files, and plots
+# Author: Marta Carrizo Vaque
+# Date: 10-03-2019
+# ===================================================================
+
+# packages
+library(dplyr)    # data wrangling
+library(ggplot2)  # graphics
+
+
+
+
+
