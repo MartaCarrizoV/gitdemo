246d662990034f70034dd6c34ac84eb0e6a5aaea
Marta Carrizo Vaque <marta.carrizo@berkeley.edu>
2019-10-03 15:01:12 -0700

    code: update demo-script.R

--- code/demo-script.R
+++ code/demo-script.R
@@ -1,8 +1,7 @@
 # ===================================================================
 # Title: Lab 05
 # Description:
-#   This script performs what it asked on
-#   lab05 of STAT 133
+#   Get familiar with git and more bash commands.
 # Input(s): data file 'nba2018-players.csv'
 # Output(s): summary data files, and plots
 # Author: Marta Carrizo Vaque
@@ -12,6 +11,65 @@
 # packages
 library(dplyr)    # data wrangling
 library(ggplot2)  # graphics
+
+#I read the data of nba2018-players.csv
+dat <- read.csv("./../data/nba2018-players.csv"
+                , sep=",")
+
+#I create a data frame of the warriors
+warriors <- dat %>% filter(team == 'GSW') %>% 
+  arrange(salary)
+
+#I export this file
+write.csv(warriors, file = "../data/warriors.csv", row.names = FALSE)
+
+#EXPORTING SOME R OUTPUTS
+
+#I sink basic statistics
+sink(file = '../output/summary-height-weight.txt')
+summary(dat[ ,c('height', 'weight')])
+sink()
+
+#I export the output of str() of dat
+sink(file="../output/data-structure.txt")
+str(dat)
+sink()
+
+#I export the summary of the warriors
+sink(file = "../output/summary-warriors.txt")
+summary(warriors)
+sink()
+
+#I create a data frame with the lakers information
+#Then I export its summary
+lakers <- filter(dat, team == 'LAL')
+sink(file = "../output/summary-lakers.txt")
+summary(lakers)
+sink()
+
+
+#EXPORTING GRAPHS
+
+#I create a scatterplot
+png(filename = "../images/scatterplot-height-weight.png")
+plot(dat$height, dat$weight, pch = 20, 
+     xlab = 'Height', ylab = 'Height')
+dev.off()
+
+#I create a histogram of the age.
+jpeg(filename = "../images/histogram-age.jpeg", width = 600, 
+     height = 400)
+hist(dat$age)
+dev.off()
+
+#I create a scatterplot of height and weight
+png(filename = "../images/scatterplot2-height-weight.png",
+    pointsize = 0.01)
+plot(dat$height, dat$weight, pch = 20, 
+     xlab = 'Height', ylab = 'Height')
+dev.off()
+
+
 
 
 
